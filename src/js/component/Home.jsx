92f1b6ec82072bc1c1133801ff5f8b700254a29d
Aaron Arismendi <arismendiaaron@gmail.com>
2021-11-18 11:47:32 -0400

    Listo

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+
+import { Task } from "./Task.jsx";
+
+const url = "https://assets.breatheco.de/apis/fake/todos/user/Aaron2301";
+
+export const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+
+	useEffect(() => {
+		const getTodoList = async () => {
+			const response = await fetch(url);
+			const body = await response.json();
+			setTasks(body);
+		};
+		getTodoList();
+	}, []);
+
+	const updateTask = async (position, checked) => {
+		let result = tasks.map((task, index) => {
+			let newTask = { ...task };
+			if (index === position) {
+				newTask.done = checked;
+			}
+
+			return newTask;
+		});
+
+		const response = await fetch(url, {
+			method: "PUT",
+			body: JSON.stringify(result),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		if (response.ok) {
+			const getResponse = await fetch(url);
+			const body = await getResponse.json();
+			setTasks(body);
+		}
+	};
+
+	const deleteTask = async position => {
+		let result = tasks.filter((task, index) => index !== position);
+		let _method = "";
+
+		_method = "PUT";
+		result = JSON.stringify(result);
+		const response = await fetch(url, {
+			method: _method,
+			body: result,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		if (response.ok) {
+			const getResponse = await fetch(url);
+			const body = await getResponse.json();
+			setTasks(body);
+		}
+	};
+
+	return (
+		<>
+			<div className="container box">
+				<div className="row">
+					<h1 className="titleStyle">To Do List</h1>
+				</div>
+				<div className="justify-content-center">
+					<div className="row">
+						<input
+							className="inputStyle"
+							type="text"
+							placeholder="write a task here"
+							value={inputValue}
+							onChange={event => {
+								setInputValue(event.target.value);
+							}}
+							onKeyPress={event => {
+								if (event.key == "Enter") {
+									if (event.target.value == "") {
+										alert("Please add some tasks");
+										return;
+									}
+									const createTask = async () => {
+										let newTask = [
+											...tasks,
+											{
+												label: event.target.value,
+												done: false
+											}
+										];
+
+										const response = await fetch(url, {
+											method: "PUT",
+											body: JSON.stringify(newTask),
+											headers: {
+												"Content-Type":
+													"application/json"
+											}
+										});
+										if (response.ok) {
+											const getResponse = await fetch(
+												url
+											);
+											const body = await getResponse.json();
+											setTasks(body);
+										}
+									};
+									createTask();
+									setInputValue("");
+								}
+							}}
+						/>
+					</div>
+					<div className="row">
+						<ul className="list-unstyled">
+							{tasks.map((task, index) => {
+								return (
+									<Task
+										key={index}
+										inputTask={task.label}
+										isChecked={task.done}
+										position={index}
+										updateTaskList={(
+											taskPosition,
+											isDone
+										) => updateTask(taskPosition, isDone)}
+										removeCallBack={_removeTask =>
+											deleteTask(_removeTask)
+										}
+									/>
+								);
+							})}
+						</ul>
+					</div>
+					<div className="placeHolderStyle">
+						{tasks.length} Task remaining
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-//import react into the bundle
+///import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -7,9 +7,9 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
+import { Home } from "./component/home.jsx";
 
 //import your own components
-import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
